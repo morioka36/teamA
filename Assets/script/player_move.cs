a5c20b1ba2bb131108374b8afa7e1d4785d7d7a5
大家　るい <2237019@ecc.ac.jp>
2024-10-14 16:35:38 +0900

    プレイヤーの移動処理

---
+++ Assets/script/player_move.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private float junptime = 2;
+    private bool isGrounded = false;
+    Rigidbody2D rbody2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //移動処理
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        // プレイヤーの位置を更新
+        transform.Translate(new Vector2(moveX,0));
+
+
+        //ジャンプ処理
+        if (Input.GetKeyDown("space") && isGrounded) 
+        {
+            Jump();
+            isGrounded = false;
+        }
+
+        void Jump()
+        {
+            // 上方向に力を加える事でジャンプする
+            rbody2D.AddForce(Vector2.up * 300);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+}
