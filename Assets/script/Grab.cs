ad59e5165f8e1a46ca3b48e56cfa1f372cdf72ea
串間 <2237021@ecc.ac.jp>
2024-10-14 15:01:29 +0900

    ブロックの移動

---
+++ Assets/script/Grab.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+
+
+    [SerializeField] private Transform grabPoint;
+    [SerializeField] private Transform rayPoint;
+    private float rayDistance = 0.2f;
+    private GameObject grabObj;
+    RaycastHit2D hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grabObj == null) 
+            {
+                hit = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
+                if (hit.collider != null && hit.collider.tag == "Box")
+                {
+                    grabObj = hit.collider.gameObject;
+                    grabObj.GetComponent<Rigidbody2D>().isKinematic = true;
+                    grabObj.transform.position = grabPoint.position;
+                    grabObj.transform.SetParent(transform);
+                }
+            }
+            else
+            {
+                grabObj.GetComponent<Rigidbody2D>().isKinematic = false;
+                grabObj.transform.SetParent(null);
+                grabObj = null;
+            }
+        }
+    }
+}
