42d8754e88863740514a53a0d92789debad4d4d3
串間 <2237021@ecc.ac.jp>
2024-10-21 14:57:20 +0900

    最新　フォルダ整理

--- Assets/Kushima/Script/Door.cs
+++ Assets/Kushima/Script/Door.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     private bool isOpen = false; // ドアが開いているかどうか
+    private bool isOpen2 = false;
 
-     public void Open()
+    public void Open()
      {
          if (!isOpen)
          {
@@ -15,6 +16,19 @@ public class Door : MonoBehaviour
             transform.position += new Vector3(0, 5, 0); // 上に移動
          }
      }
+
+    public void OpenDoor()
+    {
+        if (!isOpen2)
+        {
+            isOpen2 = true;
+            // ドアを開ける処理（例: スライド）
+            transform.position += new Vector3(1, 0, 0); // 右に1ユニット移動
+        }
+    }
+
+   
+
     public void Close()
     {
         if (isOpen)
@@ -22,6 +36,11 @@ public class Door : MonoBehaviour
             isOpen = false;
             // ドアを閉じる処理（例: Y軸で元の位置に戻す）
             transform.position -= new Vector3(0, 5, 0); // 元の位置に戻す
+
+        }
+        if (!isOpen2)
+        {
+            isOpen2 = true;
         }
     }
     // Start is called before the first frame update
